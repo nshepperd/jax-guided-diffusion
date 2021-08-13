f50aea1a0fc7a98561db46f669553f4e81acb524
nshepperd <nshepperd@gmail.com>
2021-08-14 01:34:30 +1000

    check for already downloaded file in init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ pip install dm-haiku
 git clone https://github.com/kingoflolz/CLIP_JAX
 
 # Download checkpoint
-curl -OL 'https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt'
-python convert_checkpoint.py
+test -a 256x256_diffusion_uncond.pt || curl -OL 'https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt'
+test -a 256x256_diffusion_uncond.cbor || python convert_checkpoint.py
