c95cc797f2d20f3b2158cfad3ed08b93a2a9c583
nshepperd <nshepperd@gmail.com>
2021-08-14 18:35:43 +1000

    Add more monkeypatches

--- jaxtorch/monkeypatches.py
+++ jaxtorch/monkeypatches.py
@@ -1,10 +1,19 @@
+"""Implement a bunch of convenience methods for jax arrays.
+
+"""
+
+import sys
 import jax
 import jaxlib
 import numpy as np
 import functools
+from einops import rearrange
 
 def register(**kwargs):
     for (attr, fun) in kwargs.items():
+        if (hasattr(jaxlib.xla_extension.DeviceArrayBase, attr) or hasattr(jax.core.Tracer, attr)):
+            print(f'Not monkeypatching DeviceArray and Tracer with `{attr}`, because that method is already implemented.', file=sys.stderr)
+            continue
         setattr(jaxlib.xla_extension.DeviceArrayBase, attr, fun)
         setattr(jax.core.Tracer, attr, fun)
 
@@ -12,8 +21,11 @@ register(
     square = lambda arr: arr**2,
     sqrt = lambda arr: jax.numpy.sqrt(arr),
     add = lambda a, b: a + b,
+    sub = lambda a, b: a - b,
     div = lambda a, b: a / b,
     mul = lambda a, b: a * b,
     arcsin = lambda a: jax.numpy.arcsin(a),
-    clamp = lambda a, minval, maxval: jax.numpy.clip(a, a_min=minval, a_max=maxval)
+    clamp = lambda a, minval, maxval: jax.numpy.clip(a, a_min=minval, a_max=maxval),
+    unsqueeze = lambda arr, axis=0: jax.numpy.expand_dims(arr, axis),
+    rearrange = rearrange,
 )
